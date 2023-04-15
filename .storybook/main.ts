c847f3d8b934a410630196bc9b7dfe106e4db583
brain-shutdown <brainshutdown@gmail.com>
2023-04-15 12:52:54 +0100

    Storybook updated

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,37 @@
 module.exports = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {}
-  },
-  features: {
-    storyStoreV7: true
-  },
-  docs: {
-    autodocs: true
-  }
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+		'@storybook/addon-controls',
+		{
+			name: '@storybook/addon-storysource',
+			options: {
+				loaderOptions: {
+					prettierConfig: { printWidth: 80, singleQuote: false },
+				},
+			},
+		},
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
+	features: {
+		storyStoreV7: true,
+	},
+	docs: {
+		autodocs: true,
+	},
+	typescript: {
+		check: false,
+		checkOptions: {},
+		reactDocgen: 'react-docgen-typescript',
+		reactDocgenTypescriptOptions: {
+			shouldExtractLiteralValuesFromEnum: true,
+			propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+		},
+	},
 };
